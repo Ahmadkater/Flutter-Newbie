edd04591f08b98cf9c6e57195209f763beb150a8
Ahmadkater <36134615+Ahmadkater@users.noreply.github.com>
2025-01-02 23:58:19 +0200

    update lib

--- Task3/lib/Screens/DetailsScreen.dart
+++ Task3/lib/Screens/DetailsScreen.dart
@@ -26,72 +26,156 @@ class _DetailsscreenState extends State<Detailsscreen> {
 
     return isLoading
         ? CircularProgressIndicator()
-        : Scaffold(
-            body: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(16),
-                  alignment: Alignment.bottomCenter,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [AppColors.blackColor, AppColors.blackColor],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter),
-                      image: const DecorationImage(
-                          fit: BoxFit.contain,
-                          image: AssetImage("assets/coverimage.png"),
-                          alignment: Alignment.topCenter)),
-                  child: SafeArea(
-                    child: Column(
+        : ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Scaffold(
+              backgroundColor: AppColors.blackColor,
+              body: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                              AppColors.blackColor,
+                              AppColors.blackColor
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter),
+                        image: DecorationImage(
+                            fit: BoxFit.contain,
+                            image: AssetImage("assets/coverimage.png"),
+                            alignment: Alignment.topCenter)),
+                    child: SafeArea(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(children: [
+                              IconButton(
+                                  style: ButtonStyle(
+                                      backgroundColor: WidgetStatePropertyAll(
+                                          AppColors.redColor)),
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      "ScreenOne",
+                                    );
+                                  },
+                                  icon: Icon(
+                                    Icons.arrow_back,
+                                    color: AppColors.whiteColor,
+                                  )),
+                              SizedBox(
+                                width: 100,
+                              ),
+                              Text(planet.planetName,
+                                  style: TextStyle(
+                                      color: AppColors.whiteColor,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w700))
+                            ]),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            Text(
+                              "${planet.title}",
+                              style: TextStyle(
+                                  color: AppColors.whiteColor,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700),
+                            )
+                          ]),
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.contain,
+                            image: AssetImage(
+                                "assets/Planets/${planet.planetImage}"),
+                            alignment: Alignment.topCenter)),
+                  )),
+                  SafeArea(
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Row(children: [
-                            IconButton(
-                                style: ButtonStyle(
-                                    backgroundColor: WidgetStatePropertyAll(
-                                        AppColors.redColor)),
-                                onPressed: () {
-                                  Navigator.pushNamed(
-                                    context,
-                                    "ScreenOne",
-                                  );
-                                },
-                                icon: Icon(
-                                  Icons.arrow_back,
-                                  color: AppColors.whiteColor,
-                                )),
-                            SizedBox(
-                              width: 100,
-                            ),
-                            Text(planet.planetName,
-                                style: TextStyle(
-                                    color: AppColors.whiteColor,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w700))
-                          ]),
-                          SizedBox(
-                            height: 40,
-                          ),
                           Text(
-                            "${planet.title}",
+                            "About",
                             style: TextStyle(
                                 color: AppColors.whiteColor,
                                 fontSize: 24,
                                 fontWeight: FontWeight.w700),
-                          )
-                        ]),
+                          ),
+                          SafeArea(
+                            minimum: EdgeInsets.only(top: 8, bottom: 16),
+                            child: Text(
+                              overflow: TextOverflow.visible,
+                              planet.about,
+                              style: TextStyle(
+                                  color: AppColors.whiteColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                          ),
+                          Text(
+                            "Distance From Sun(Km): " + planet.dfe,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Length of Day(hours): " + planet.lod,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Orbital Period(Earth Years): " + planet.op,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Radius(km): " + planet.radius,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Mass(kg): " + planet.mass,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Gravity(m/s²): " + planet.gravity,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "Surface Area(km²): " + planet.surfaceArea,
+                            style: TextStyle(
+                                color: AppColors.whiteColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                Image.asset("assets/Planets/${planet.planetImage}"),
-                Text(
-                  "About",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-                ),
-                Text(
-                  planet.about,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
-                ),
-              ],
+                ],
+              ),
             ),
           );
   }
